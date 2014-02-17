8975b679e668603da9770b8fee4a20defe5452fa
ericmell <ms.wenching@qq.com>
2014-02-17 10:57:23 +0800

    test h2
    this code is used to test h2

---
+++ src/JavaH2.java
@@ -0,0 +1,33 @@
+import java.sql.*;  
+  
+public class JavaH2 {  
+    public void query(String SQL) {  
+        try {  
+            String sourceURL = "jdbc:h2:E:/h2db/test";  
+            String user = "sa";  
+            String key = "";  
+  
+            try {  
+                Class.forName("org.h2.Driver");  
+            } catch (Exception e) {  
+                e.printStackTrace();  
+            }  
+            Connection conn = DriverManager.getConnection(sourceURL, user, key);  
+            Statement stmt = conn.createStatement();  
+            ResultSet rset = stmt.executeQuery(SQL);  
+            while (rset.next()) {  
+                System.out.println(rset.getString("name")+ "  "+rset.getString("sex"));  
+            }  
+            rset.close();  
+            stmt.close();  
+            conn.close();  
+        } catch (SQLException sqle) {  
+            System.err.println(sqle);  
+        }  
+    }  
+  
+    public static void main(String args[]) {  
+    	JavaH2 tt = new JavaH2();  
+        tt.query("select * from mytable");  
+    }  
+}  
